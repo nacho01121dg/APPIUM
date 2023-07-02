5c954bdb95f754b69c5e5bdf97f448e025068417
Ignacio Alonso de la Cruz Garcia <nachouthh@gmail.com>
2023-07-02 13:44:20 -0600

    Add complete project APPIUM

---
+++ Behave/steps/shopping_cart_steps.py
@@ -0,0 +1,45 @@
+from behave import When, Then
+from screens.products_screen import ProductosScreen
+from screens.product_detail_screen import ProductDetailScreen
+from screens.shopping_cart_screen import ShoppingCartScreen
+from utils.dictionaries.input_data import PRODUCT_TEXTS, CART_TEXTS
+
+
+@When("Elijo 'producto'")
+def step_impl(context):
+    products_screen = ProductosScreen(context)
+    products_screen.tap_element(*products_screen.scroll_down_product_name)
+
+
+@When("Elijo 'Añadir a carrito'")
+def step_impl(context):
+    product_detail_screen = ProductDetailScreen(context)
+    product_detail_screen.tap_element(*product_detail_screen.scroll_down_product_add_cart)
+
+
+@When("Elijo el 'carrito de compra'")
+def step_impl(context):
+    product_detail_screen = ProductDetailScreen(context)
+    product_detail_screen.tap_element(*product_detail_screen.opc_carrito)
+
+
+@Then("Visualizo el carrito")
+def step_impl(context):
+    text_titulo_tu_carrito = CART_TEXTS.get("txt_title_cart")
+    shopping_cart_screen = ShoppingCartScreen(context)
+    shopping_cart_screen.assert_text(*shopping_cart_screen.title_tu_carrito, text=text_titulo_tu_carrito)
+
+
+@Then("Visualizo el 'nombre' del producto agregado al carrito")
+def step_impl(context):
+    text_nombre_producto = PRODUCT_TEXTS.get("txt_productname")
+    shopping_cart_screen = ShoppingCartScreen(context)
+    shopping_cart_screen.assert_text(*shopping_cart_screen.lbl_nombre_producto, text=text_nombre_producto)
+
+
+@Then("Visualizo el 'precio' del producto agregado al carrito")
+def step_impl(context):
+    text_precio_producto = PRODUCT_TEXTS.get("txt_price")
+    shopping_cart_screen = ShoppingCartScreen(context)
+    shopping_cart_screen.assert_text(*shopping_cart_screen.lbl_precio_producto,
+                                   text=text_precio_producto)
---
+++ Behave/utils/dictionaries/input_data.py
@@ -0,0 +1,22 @@
+LOGIN_TEXTS = {
+    'txt_username': "standard_user",
+    'txt_password': "secret_sauce",
+    'text_homepage': "PRODUCTOS"
+}
+
+PRODUCT_TEXTS = {
+    'txt_title_product_detail': "REGRESO A PRODUCTOS",
+    'contest_desc_cart': "test-Carrito",
+    'txt_add_cart': "AÑADIR A CARRITO",
+    'txt_productname': "Mameluco Sauce Labs",
+    'txt_price': "$7.99"
+}
+
+CART_TEXTS = {
+    'txt_title_cart': "TU CARRITO"
+}
+
+ORDER_BY_PRICE_TEXTS = {
+    'txt_precio_menor': "$7.99",
+    'txt_precio_mayor': "$49.99"
+}
